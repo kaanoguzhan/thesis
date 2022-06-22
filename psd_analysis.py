102aaacec1a615d838b22a3991494d0b2cdaf34d
kaan.oguzhan <kaan.oguzhan@tum.de>
2022-06-22 02:21:39 +0200

    State of code as of 13.06.2022 Meeting including format fixes

--- psd_analysis.py
+++ psd_analysis.py
@@ -5,8 +5,6 @@
 # Imports
 # ────────────────────────────────────────────────────────────
 import argparse
-import io
-import os
 import sys
 import warnings
 from datetime import datetime
@@ -14,7 +12,6 @@ from datetime import datetime
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-from matplotlib.ticker import FormatStrFormatter
 from torch.utils.tensorboard import SummaryWriter
 
 from utils.awake_data_loader import AWAKE_DataLoader
@@ -23,26 +20,26 @@ from utils.beam_utils import (find_marker_laser_pulse,
 from utils.general_utils import in_ipynb
 from utils.image_utils import split_image_with_stride
 
-if in_ipynb():
-    sys.argv = ['']
-else:
-    matplotlib.use('Agg')
 
 # ─────────────────────────────────────────────────────────────
-# Constants
+# Default Constants
 # ─────────────────────────────────────────────────────────────
-
+# TODO : Add "DEFAULT_" prefix to all constants
 DEFAULT_BEAM_WINDOW = 100
-SPLIT_WINDOW_SIZE = 10
-SPLIT_STRIDE = 10
-PSD_CUTOFF = (0.9, 0.01)
-PADDING_MULTIPLIER = 5
-
-print(f'Using Jupyter Notebook:{in_ipynb()}')
+DEFAULT_SPLIT_WINDOW_SIZE = 10
+DEFAULT_SPLIT_STRIDE = 10
+DEFAULT_PSD_CUTOFF = (3, 0.05)
+DEFAULT_PADDING_MULTIPLIER = 50
 
 # ─────────────────────────────────────────────────────────────
 # Settings
 # ─────────────────────────────────────────────────────────────
+
+print(f'Using Jupyter Notebook:{in_ipynb()}')
+if in_ipynb():
+    sys.argv = ['']
+else:
+    matplotlib.use('Agg')
 
 warnings.filterwarnings("ignore")
 
@@ -56,36 +53,34 @@ plt.rcParams.update({
 parser = argparse.ArgumentParser(description='FFT denoise analysis')
 parser.add_help = True
 parser.add_argument('--beam_window', type=int, default=DEFAULT_BEAM_WINDOW, help='Beam Windows width around the beam center')
-parser.add_argument('--split_window_size', type=int, default=SPLIT_WINDOW_SIZE, help='TODO')
-parser.add_argument('--split_stride', type=int, default=SPLIT_STRIDE, help='TODO')
-parser.add_argument('--psd_cutoff', type=int, default=PSD_CUTOFF, help='TODO')
-parser.add_argument('--padding_multiplier', type=int, default=PADDING_MULTIPLIER, help='TODO')
+parser.add_argument('--split_window_size', type=int, default=DEFAULT_SPLIT_WINDOW_SIZE, help='TODO')
+parser.add_argument('--split_stride', type=int, default=DEFAULT_SPLIT_STRIDE, help='TODO')
+parser.add_argument('--psd_cutoff', type=int, default=DEFAULT_PSD_CUTOFF, help='TODO')
+parser.add_argument('--padding_multiplier', type=int, default=DEFAULT_PADDING_MULTIPLIER, help='TODO')
 args = parser.parse_args()
 
-# Sets up log directory with timestamp
-# current_time = datetime.now().strftime("%Y.%m.%d-%H_%M_%S")
+# Set up log directory with timestamp and create file writer
 current_time = datetime.now().strftime("%Y.%m.%d")
 logdir = f'logs/{current_time}_AWAKE_PSD_Analysis' +\
-    f'_window{args.beam_window}' +\
-    f'_split{SPLIT_WINDOW_SIZE}-{SPLIT_STRIDE}' +\
-    f'_PSDCutoff{args.psd_cutoff[0]}-{args.psd_cutoff[1]}' +\
-    f'_Padding{args.padding_multiplier}'
-# Creates a file writer for the log directory.
-file_writer_1 = SummaryWriter(logdir)
-# file_writer_2 = tf.summary.create_file_writer(logdir)
+    f'_window{args.beam_window:>03d}' +\
+    f'_split{DEFAULT_SPLIT_WINDOW_SIZE:>02d}' +\
+    f'_stride{DEFAULT_SPLIT_STRIDE:>02d}' +\
+    f'_PSDCutoff{args.psd_cutoff[0]:>.2f}-{args.psd_cutoff[1]:>.4f}' +\
+    f'_Padding{args.padding_multiplier:>02d}'
+file_writer_1 = SummaryWriter(f'{logdir}/tensorboard_logs')
 
 
 print(f'\
-    Parameters:\n\
-    ----------------------------------------\n\
+Parameters:\n\
+----------------------------------------\n\
     Beam window: {args.beam_window}\n\
-    Split window size: {SPLIT_WINDOW_SIZE}\n\
-    Split stride: {SPLIT_STRIDE}\n\
+    Split window size: {DEFAULT_SPLIT_WINDOW_SIZE}\n\
+    Split stride: {DEFAULT_SPLIT_STRIDE}\n\
     PSD Cutoff: {args.psd_cutoff}\n\
     0-Padding: {args.padding_multiplier}\n\
     Log directory: {logdir}\n\
-    ----------------------------------------\n\
-    ')
+----------------------------------------\n\
+')
 
 # %% ─────────────────────────────────────────────────────────────────────────────
 # Image loading and splitting into sub-images
@@ -98,6 +93,7 @@ streak_img = adl.data[0].streak_image
 # plot streak_img as a sanity check
 plt.figure()
 plt.imshow(streak_img, vmax=5000)
+plt.savefig(f'{logdir}/0_streak_img.pdf', format='pdf', bbox_inches='tight', pad_inches=0.1)
 print(f'Streak image shape: {streak_img.shape}')
 
 streak_img = streak_img.T
@@ -105,70 +101,78 @@ streak_img = streak_img.T
 # Crop image from the beginning of Marker Laser Pulse
 mlp_start, mlp_end = find_marker_laser_pulse(streak_img, x_axis_is_space=True)
 print(f'Marker laser pulse is between: {mlp_start, mlp_end}')
-streak_img = streak_img[:, mlp_start:]
+streak_img = streak_img[:, 300:]
+# streak_img = streak_img[:, mlp_end:]
 # plot the streak_img after the Marker Laser Pulse cutting as a sanity check
-plt.figure(figsize=(15, 10))
+plt.figure()
 plt.imshow(streak_img, vmax=5000)
+plt.savefig(f'{logdir}/1_streak_img_after_mlp_cut.pdf', format='pdf', bbox_inches='tight', pad_inches=0.1)
 
 # Cut a window around the beam center and polot it as a sanity check
 beam_window_start, beam_window_end = get_window_around_beam_center(streak_img, args.beam_window, x_axis_is_space=True)
+# beam_center = streak_img[290:300, :]
 beam_center = streak_img[beam_window_start:beam_window_end, :]
 plt.figure(figsize=(15, 10))
 plt.imshow(beam_center, vmax=5000)
+plt.savefig(f'{logdir}/2_streak_img_after_mlp_cut_beam_window.pdf', format='pdf', bbox_inches='tight', pad_inches=0.1)
 
 tmp_beam_center = beam_center.copy()
-psd_cutoff = 0.1
 
 # Split image into sub-images with windows size of SPLIT_WINDOW_SIZE and stride of SPLIT_STRIDE
-img_splits = split_image_with_stride(image=tmp_beam_center, window_size=SPLIT_WINDOW_SIZE, stride=SPLIT_STRIDE)
+img_splits = split_image_with_stride(image=tmp_beam_center, window_size=DEFAULT_SPLIT_WINDOW_SIZE, stride=DEFAULT_SPLIT_STRIDE)
 
-# Concat all images in img_split on top of eachother and leave 5 zeros layer between each image
+# Concat all images in img_split on top of eachother, leave 5 pixel wide zeros between each image
+# Then plot it as a sanity check
 img_cat = np.zeros(((img_splits[0].shape[0]+5) * len(img_splits) - 5, img_splits[0].shape[1]))
-
 for idx, img_split in enumerate(img_splits):
     img_cat[idx*(img_splits[0].shape[0])+(idx)*5:(idx+1)*(img_splits[0].shape[0])+(idx)*5, :] = img_split
-
-# Plot it for sanity check
 plt.figure(figsize=(15, 10))
 plt.imshow(img_cat, vmax=5000)
+plt.savefig(f'{logdir}/3_streak_img_after_mlp_cut_beam_window_split.pdf', format='pdf', bbox_inches='tight', pad_inches=0.1)
 
 
 # %% ─────────────────────────────────────────────────────────────────────────────
 # Applying FFT filter and then doing the PSD analysis
 # ────────────────────────────────────────────────────────────────────────────────
-def fft_filter_img(img, psd_cutoff, fft_bin_multiplier, plot=False, return_plot=False):
+
+def fft_filter_img(img, psd_cutoff, fft_bin_multiplier, psd_frequency_range=(15,200)):
     """
     Returns a fft filtered image.
     """
+    dt_ps = 210 / 512  # Picoseconds (total time window:210 ps over 512 pixels)
+    dt_fs = dt_ps * 1000 # Femtoseconds, this is to keep the precision of the frequency later on
 
     # Save image shape and row by sum on the first dimension
     img_shape = img.shape
     signal = np.sum(img, axis=0)
+    signal_org = signal.copy()
+    signal_org = signal_org - np.mean(signal_org)  # detrend signal
 
     # Normalize signal between 0 and 1
     signal_max = np.max(signal)
     signal_min = np.min(signal)
     signal = (signal - signal_min) / (signal_max - signal_min)
-    signal_org = signal.copy()
+    
+    signal = signal - np.mean(signal)  # detrend signal
 
     t_exp = np.arange(len(signal), dtype=np.int32)
 
-    # Compute Fourier Coefficients of the f_sample signal
-    # Fourier Coefficients have magnitude and phase
+    # ─────────────────────────────────────────────────────────────
+    # Use PSD to filter out noise
+    # ─────────────────────────────────────────────────────────────
+
+    # Compute Fourier Coefficients of the sample signal
     Fn = np.fft.fft(signal, len(signal)*fft_bin_multiplier)
 
-    # Use PSD to filter out noise
-
     # Compute Power Spectral Denisty
-    p_s_d = Fn * np.conj(Fn) / len(signal)
+    p_s_d = (np.real(Fn)**2 + np.imag(Fn)**2) / len(signal)
 
-    # Normalize PSD
-    p_s_d_min = np.min(p_s_d)
-    p_s_d_max = np.max(p_s_d)
-    p_s_d = (p_s_d - p_s_d_min) / (p_s_d_max - p_s_d_min)
+    # # Normalize PSD
+    # p_s_d_min = np.min(p_s_d)
+    # p_s_d_max = np.max(p_s_d)
+    # p_s_d = (p_s_d - p_s_d_min) / (p_s_d_max - p_s_d_min)
 
-    # Find all frequencies with large power
-    # Check if psd_cutoff is a number or a tuple
+    # Apply PSD cutoff
     if isinstance(psd_cutoff, tuple):
         psd_cutoff_max = psd_cutoff[0]
         psd_cutoff_min = psd_cutoff[1]
@@ -178,102 +182,112 @@ def fft_filter_img(img, psd_cutoff, fft_bin_multiplier, plot=False, return_plot=
     indices = np.logical_and(p_s_d > psd_cutoff_min, p_s_d < psd_cutoff_max)
 
     # Zero out smaller Fourier coefficients and their corresponding frequencies
-    # Boradcast Fn to the same shape as img_shape
     p_s_d_clean = p_s_d * indices
-
-    # Clean p_s_d and p_s_d_Clean from imaginary part
-    p_s_d_clean = np.real(p_s_d_clean)
-    p_s_d = np.real(p_s_d)
 
     # Go over each row on image and replace with PSD filtered signal
     for i in range(img_shape[0]):
         signal_row = img[i]
+        signal_row = signal_row - np.mean(signal_row)
         Fn_row = np.fft.fft(signal_row, len(signal_row)*fft_bin_multiplier)
         Fn_row = Fn_row * indices
-        img_ifft = np.fft.ifft(Fn_row)
-        img[i] = np.real(img_ifft[:img_shape[1]])
+        img_row_ifft = np.fft.ifft(Fn_row)
+        img[i] = np.real(img_row_ifft[:img_shape[1]])
 
     signal_clean = np.sum(img, axis=0)
-    # signal_clean_max = np.max(signal_clean)
-    # signal_clean_min = np.min(signal_clean)
-    # signal_clean = (signal_clean - signal_clean_min) / (signal_clean_max - signal_clean_min)
 
-    if plot:
-        # Plotting denoising results
-        fig, axs = plt.subplots(4, 1, figsize=(20, 22))
-        fig.tight_layout(rect=[0, 0.03, 1, 0.95])
-        plt.subplots_adjust(hspace=0.2)
+    ghz_freqs = np.power(dt_fs, -1) * 1000 * 1000 # dt_fs^-1 *1000*1000 = PetaHz *1000*1000 => GigaHz 
+    freq_ticks = np.fft.fftfreq(len(signal)*fft_bin_multiplier) * ghz_freqs
+    
+    frequency_range_start = np.where(freq_ticks > psd_frequency_range[0])[0][0]
+    frequency_range_end = np.where(freq_ticks > psd_frequency_range[1])[0][0]
 
-        plt.sca(axs[0])
-        plt.plot(t_exp, signal_org, color='k')
-        plt.plot(t_exp, signal_org, 'o', color='k', label='Noisy signal')
-        plt.xlim(t_exp[0], t_exp[-1])
-        plt.title('Noisy signal')
-        plt.ylim(np.min(signal_org), np.max(signal_org))
-        plt.grid()
-        plt.legend()
+    # Set all frequencies that are not in psd_frequency_range to None
+    p_s_d[:frequency_range_start] = None
+    p_s_d[frequency_range_end:] = None
+    p_s_d_clean[:frequency_range_start] = None
+    p_s_d_clean[frequency_range_end:] = None
 
-        plt.sca(axs[1])
-        plt.plot(t_exp, p_s_d[t_exp], color='b')
-        plt.plot(t_exp, p_s_d[t_exp], 'o', color='b', linewidth=2, label='Noisy signal')
-        plt.plot([t_exp[0], t_exp[-1]], [psd_cutoff_min, psd_cutoff_min], '--', color='tab:orange', label='PSD cutoff')
-        plt.plot([t_exp[0], t_exp[-1]], [psd_cutoff_max, psd_cutoff_max], '--', color='tab:orange', label='PSD cutoff')
-        plt.xlim(t_exp[0], t_exp[-1])
-        # plt.xlim(freq[0], 40)
-        # plt.ylim(0, psd_cutoff*10)
-        axs[1].yaxis.set_major_formatter(FormatStrFormatter('%.6f'))
-        plt.title(f'Power Spectral Density')
-        plt.xlabel('Frequency')
-        plt.ylabel('Power Spectrum |Fn|^2')
-        plt.legend()
+    # fig, ax = plt.subplots()
+    # ax.plot(freq_ticks, p_s_d)
+    # ax.plot(freq_ticks, p_s_d, 'o', color='tab:blue', markersize=3)
+    # ax.set_xlabel('Frequency [GHz]')
+    # ax.set_ylabel('Power')
+    # # plt.xticks(range(0, psd_frequency_range[-1]+10, 10))
+    # # plt.xlim()
+    # ax.set_xlim(0, psd_frequency_range[-1]+20)
+    # ax.set_ylim(0, 1.05)
+    # ax.set_xticks(range(0, 250, 10))
+    # ax.grid()
+    # fig.savefig(f'plot_PSD.pdf')
 
-        plt.sca(axs[2])
-        plt.plot(t_exp, p_s_d[t_exp], color='b')
-        plt.plot(t_exp, p_s_d_clean[t_exp], color='darkred')
-        plt.plot(t_exp, p_s_d[t_exp], 'o', color='b', linewidth=2, label='Noisy signal')
-        plt.plot(t_exp, p_s_d_clean[t_exp], 'o', color='darkred', linewidth=2, label='Filtered signal')
-        plt.plot([t_exp[0], t_exp[-1]], [psd_cutoff_min, psd_cutoff_min], '--', color='tab:orange', label='PSD cutoff')
-        plt.plot([t_exp[0], t_exp[-1]], [psd_cutoff_max, psd_cutoff_max], '--', color='tab:orange', label='PSD cutoff')
-        plt.xlim(t_exp[0], 200)
-        plt.ylim(0, psd_cutoff_min*10)
-        axs[2].yaxis.set_major_formatter(FormatStrFormatter('%.7f'))
-        plt.title(f'Power Spectral Density after filtering (PSD cutoff = {psd_cutoff_min:0.9f})')
-        plt.xlabel('Frequency')
-        plt.ylabel('Power Spectrum |Fn|^2')
-        plt.legend()
+    # Calculate Time and Frequency Ticks
+    time_ticks = np.arange(0, t_exp[-1], 1) * dt_fs
+    time_ticks = [int(i) for i in time_ticks]  # in Femtoseconds
+    
+    ghz_freqs = np.power(dt_fs, -1) * 1000 * 1000 # dt_fs^-1 *1000*1000 = PetaHz *1000*1000 => GigaHz
+    freq_ticks = np.fft.fftfreq(len(signal)*fft_bin_multiplier) * ghz_freqs
 
-        plt.sca(axs[3])
-        plt.plot(t_exp, signal_org, 'o', color='k', markersize=5, label='Noisy signal')
-        plt.plot(t_exp, signal_clean, 'o', color='r', markersize=5, label='Filtered signal')
-        # plt.plot(t_original, f_original, color='g', label='Original signal')
-        plt.xlim(t_exp[0], t_exp[-1])
-        plt.title('Filtered signal vs Original signal')
-        plt.grid()
-        plt.legend()
+    # Plotting denoising results
+    fig, axs = plt.subplots(4, 1, figsize=(20, 22))
+    fig.tight_layout(rect=[0, 0.03, 1, 0.95])
+    plt.subplots_adjust(hspace=0.2)
 
-        fig.suptitle(f'Signal PSD Filtering with PSD cutoff = {psd_cutoff_min:0.9f}', fontsize=16)
+    plt.sca(axs[0])
+    plt.plot(t_exp, signal_org, color='k')
+    plt.plot(t_exp, signal_org, 'o', color='k', label='Noisy signal')
+    plt.xlim(t_exp[0], t_exp[-1])
+    plt.xticks(range(0, len(time_ticks), 10), time_ticks[::10])
+    plt.ylim(np.min(signal_org), np.max(signal_org))
+    plt.title('Noisy signal (mean removed)')
+    plt.xlabel('Time [fs]')
+    plt.ylabel('Pixel Intensity (Column Sum)')
+    plt.grid()
+    plt.legend()
 
-        if return_plot:
-            buf1 = io.BytesIO()
-            plt.savefig(buf1, format='png', bbox_inches='tight',  dpi=45)
-            plt.savefig(f'{logdir}/denoise_psd_graphs_{psd_cutoff_min:0.9f}.pdf', format='pdf', bbox_inches='tight',  dpi=100)
-            buf1.seek(0)
-            plt.show()
-            buf1.close()
-            return img, signal_org, signal_clean, p_s_d, p_s_d_clean
-        else:
-            plt.show()
+    plt.sca(axs[1])        
+    plt.plot(freq_ticks, p_s_d, color='b')
+    plt.plot(freq_ticks, p_s_d, 'o', color='b', linewidth=2, label='Noisy signal')
+    plt.plot([psd_frequency_range[0], psd_frequency_range[-1]], [psd_cutoff_min, psd_cutoff_min], '--', color='tab:orange', label='PSD cutoff')
+    plt.plot([psd_frequency_range[0], psd_frequency_range[-1]], [psd_cutoff_max, psd_cutoff_max], '--', color='tab:orange', label='PSD cutoff')
+    plt.xticks(range(0, psd_frequency_range[-1]+10, 10))
+    plt.xlim(0, psd_frequency_range[-1])
+    plt.ylim(0, 2)
+    plt.title(f'Power Spectral Density')
+    plt.xlabel('Frequency [GHz]')
+    plt.ylabel('Power')
+    plt.grid()
+    plt.legend()
 
-    return img, signal_org, signal_clean, p_s_d, p_s_d_clean
+    plt.sca(axs[2])
+    plt.plot(freq_ticks, p_s_d, color='b')
+    plt.plot(freq_ticks, p_s_d_clean, color='darkred')
+    plt.plot(freq_ticks, p_s_d, 'o', color='b', linewidth=2, label='Noisy signal')
+    plt.plot(freq_ticks, p_s_d_clean, 'o', color='darkred', linewidth=2, label='Filtered signal')
+    plt.plot([psd_frequency_range[0], psd_frequency_range[-1]], [psd_cutoff_min, psd_cutoff_min], '--', color='tab:orange', label='PSD cutoff')
+    plt.plot([psd_frequency_range[0], psd_frequency_range[-1]], [psd_cutoff_max, psd_cutoff_max], '--', color='tab:orange', label='PSD cutoff')
+    plt.xticks(range(0, psd_frequency_range[-1]+10, 10))
+    plt.xlim(0, psd_frequency_range[-1])
+    plt.ylim(0, 2)
+    plt.title(f'Power Spectral Density after filtering (PSD cutoff = {psd_cutoff_min:0.9f})')
+    plt.xlabel('Frequency [GHz]')
+    plt.ylabel('Power')
+    plt.grid()
+    plt.legend()
 
+    plt.sca(axs[3])
+    plt.plot(t_exp, signal_org, 'o', color='k', markersize=5, label='Noisy signal')
+    plt.plot(t_exp, signal_clean, 'o', color='r', markersize=5, label='Filtered signal')
+    plt.xlim(t_exp[0], t_exp[-1])
+    plt.xticks(range(0, len(time_ticks), 10), time_ticks[::10])
+    plt.title('Filtered signal vs Original signal (mean removed)')
+    plt.xlabel('Time [fs]')
+    plt.ylabel('Pixel Intensity (Column Sum)')
+    plt.grid()
+    plt.legend()
 
-def zero_pad_1D(img, pad_size):
-    """
-    Zero pad 1D array
-    """
-    img_pad = np.zeros((img.shape[0] + pad_size))
-    img_pad[:img.shape[0]] = img
-    return img_pad
+    fig.suptitle(f'Signal PSD Filtering with PSD cutoff ({psd_cutoff_min:0.4f},{psd_cutoff_max:0.4f})', fontsize=16)
+
+    return img, time_ticks, signal_org, signal_clean, freq_ticks, p_s_d, p_s_d_clean, plt
 
 
 def zero_pad_image(img, pad_size):
@@ -289,9 +303,38 @@ all_results = []
 
 for i in range(len(img_splits)):
     print(f'Processing image {i+1}/{len(img_splits)}')
-    # for i in [7,8,9]:
     img_input = img_splits[i].copy()
-    # img_input = zero_pad_image(img_input, pad_size=img_input.shape[1] * PADDING_MULTIPLIER)
+
+    fft_results = fft_filter_img(img_input.copy(), DEFAULT_PSD_CUTOFF, fft_bin_multiplier=DEFAULT_PADDING_MULTIPLIER)
+    img, time_ticks, signal_org, signal_clean, freq_ticks, p_s_d, p_s_d_clean, fft_signal_plot = fft_results
+
+    fft_signal_plot.savefig(f'{logdir}/denoise_psd_graphs_{i}.pdf', bbox_inches='tight')
+
+    all_results.append([
+        np.array(signal_org), 
+        np.array(signal_clean),
+        np.array(p_s_d),
+        np.array(p_s_d_clean),
+        np.array(freq_ticks)
+    ])    
+
+    # TODO saving results to tensorboard
+    # for j in range(len(time_ticks)):
+    #     file_writer_1.add_scalars(main_tag='Signal',
+    #                               tag_scalar_dict={
+    #                                   f'Original{i}': signal_org[j],
+    #                                   f'Clean{i}': signal_clean[j],
+    #                               },
+    #                               global_step=time_ticks[j])
+    # for j in range(len(freq_ticks)):
+    #     file_writer_1.add_scalars(main_tag='PSD',
+    #                               tag_scalar_dict={
+    #                                   f'Original{i}': p_s_d[j],
+    #                                   f'Clean{i}': p_s_d_clean[j],
+    #                               },
+    #                               global_step=freq_ticks[j])
+
+
 
     fft_results = fft_filter_img(img_input, PSD_CUTOFF, fft_bin_multiplier=10, plot=True, return_plot=True)
 
